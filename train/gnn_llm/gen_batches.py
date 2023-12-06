5f8a17dd4fa87005dc79837b9d7f69a15809b48b
Adrian Mladenic Grobelnik <52002338+AMGrobelnik@users.noreply.github.com>
2023-12-06 15:09:09 +0100

    idk2

--- train/gnn_llm/gen_batches.py
+++ train/gnn_llm/gen_batches.py
@@ -3,8 +3,8 @@ import pickle
 import os
 import random
 
-split_ratio = [0.8, 0.1, 0.1]
-batch_size = 5
+split_ratio = [1.0, 0.0, 0.0]
+batch_size = 1
 input_directory = "../../data/graphs/neighborhood_sampling/"
 output_directory = "../../data/graphs/batches/"
 
@@ -13,30 +13,42 @@ random.shuffle(pickle_files)
 
 num_batches = len(pickle_files) / batch_size
 
+print(len(pickle_files))
+
 for i in range(0, len(pickle_files), batch_size):
+    print(i)
     batch_num = i / batch_size
-    
-    batch = pickle_files[i: i + batch_size]
+
+    batch = pickle_files[i : i + batch_size]
 
     combined_graph = nx.DiGraph()
 
     for file_name in batch:
         file_path = os.path.join(input_directory, file_name)
-        with open(file_path, 'rb') as f:
+        with open(file_path, "rb") as f:
             current_graph = pickle.load(f)
-            combined_graph = nx.compose(combined_graph, current_graph)  
+            combined_graph = nx.compose(combined_graph, current_graph)
+            print(combined_graph.number_of_nodes())
+
+    if os.path.exists(os.path.join(output_directory, "/train")) == False:
+        os.mkdir(os.path.join(output_directory, "/train"))
+    if os.path.exists(os.path.join(output_directory, "/val")) == False:
+        os.mkdir(os.path.join(output_directory, "/val"))
+    if os.path.exists(os.path.join(output_directory, "/test")) == False:
+        os.mkdir(os.path.join(output_directory, "/test"))
 
     if batch_num < split_ratio[0] * num_batches:
-        with open(os.path.join(output_directory, "/train/{batch_num}_train.pkl"), "wb") as f:
+        with open(
+            os.path.join(output_directory, "/train/{batch_num}_train.pkl"), "wb"
+        ) as f:
             pickle.dump(combined_graph, f)
     elif batch_num < (split_ratio[0] + split_ratio[1]) * num_batches:
-        with open(os.path.join(output_directory, "/val/{batch_num}_val.pkl"), "wb") as f:
+        with open(
+            os.path.join(output_directory, "/val/{batch_num}_val.pkl"), "wb"
+        ) as f:
             pickle.dump(combined_graph, f)
     else:
-        with open(os.path.join(output_directory, "/test/{batch_num}_test.pkl"), "wb") as f:
+        with open(
+            os.path.join(output_directory, "/test/{batch_num}_test.pkl"), "wb"
+        ) as f:
             pickle.dump(combined_graph, f)
-       
-
-
-
-
