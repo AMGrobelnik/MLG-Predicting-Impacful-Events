ad2fffead47b74040bb0a65990e4ee6f7482f897
Adrian Mladenic Grobelnik <52002338+AMGrobelnik@users.noreply.github.com>
2023-12-10 12:08:21 +0100

    add split batches for each variation

--- preprocess/graph/batch_split.py
+++ preprocess/graph/batch_split.py
@@ -3,33 +3,32 @@ import shutil
 import glob
 import random
 
-# Navigate two levels up from the current script's directory
-base_path = os.path.dirname(os.path.dirname(os.getcwd()))
 
-# Set the directory paths
-base_dir = os.path.join(base_path, "data/graphs/batches")
-train_dir = os.path.join(base_dir, "train")
-val_dir = os.path.join(base_dir, "val")
-test_dir = os.path.join(base_dir, "test")
-
-# Split ratios
-train_split = 0.8
-val_split = 0.1
-test_split = 0.1
 
 def clear_directory(directory):
     """Delete all .pkl files in the specified directory"""
     for file in glob.glob(os.path.join(directory, "*.pkl")):
         os.remove(file)
 
-def split_files():
+def create_splits_for_folder(folder_name):
+    """Create train, val, test splits for a given folder"""
+    folder_path = os.path.join(base_dir, folder_name)
+    train_dir = os.path.join(folder_path, "train")
+    val_dir = os.path.join(folder_path, "val")
+    test_dir = os.path.join(folder_path, "test")
+
+    # Make sure train, val, and test directories exist
+    os.makedirs(train_dir, exist_ok=True)
+    os.makedirs(val_dir, exist_ok=True)
+    os.makedirs(test_dir, exist_ok=True)
+
     # Clear existing files in the directories
     clear_directory(train_dir)
     clear_directory(val_dir)
     clear_directory(test_dir)
 
-    # Find all .pkl files in the base directory
-    all_files = glob.glob(os.path.join(base_dir, "*.pkl"))
+    # Find all .pkl files in the folder
+    all_files = glob.glob(os.path.join(folder_path, "*.pkl"))
     random.shuffle(all_files) # Shuffle for random split
 
     # Calculate split indices
@@ -49,5 +48,19 @@ def split_files():
     for file in test_files:
         shutil.copy(file, test_dir)
 
-# Run the file splitting function
-split_files()
+
+# Set the base directory using relative paths
+base_dir = os.path.join("../../data/graphs/batches")
+
+# List of new folders to process
+folders = ["dim_reduced_10", "dim_reduced_100", "gnn_only", "llm_embeddings_full"]
+
+# Split ratios
+train_split = 0.8
+val_split = 0.1
+test_split = 0.1
+
+# Process each folder
+create_splits_for_folder("dim_reduced_10")
+# for folder in folders:
+    # create_splits_for_folder(folder)
