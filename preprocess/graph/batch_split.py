faab2e6b73a6868eceb12057aa3d440d10d67822
AdrianMG <52002338+AMGrobelnik@users.noreply.github.com>
2023-12-11 23:22:50 +0100

    split only a subset of batches

--- preprocess/graph/batch_split.py
+++ preprocess/graph/batch_split.py
@@ -10,7 +10,14 @@ def clear_directory(directory):
         os.remove(file)
 
 
-def create_splits_for_folder(base_dir, folder_name, train_split=0.8, val_split=0.1, test_split=0.1, sample_fraction=1.0):
+def create_splits_for_folder(
+    base_dir,
+    folder_name,
+    train_split=0.8,
+    val_split=0.1,
+    test_split=0.1,
+    sample_fraction=1.0,
+):
     """Create train, val, test splits for a given folder, with an option to sample a fraction of the data"""
     folder_path = os.path.join(base_dir, folder_name)
     train_dir = os.path.join(folder_path, "train")
@@ -38,11 +45,12 @@ def create_splits_for_folder(base_dir, folder_name, train_split=0.8, val_split=0
     # Calculate split indices
     train_end = int(len(sampled_files) * train_split)
     val_end = train_end + int(len(sampled_files) * val_split)
+    test_end = train_end + val_end + int(len(sampled_files) * test_split)
 
     # Split files
     train_files = sampled_files[:train_end]
     val_files = sampled_files[train_end:val_end]
-    test_files = sampled_files[val_end:]
+    test_files = sampled_files[val_end:test_end]
 
     # Copy files to respective directories
     for file in train_files:
@@ -66,6 +74,8 @@ if __name__ == "__main__":
     test_split = 0.1
 
     # Process each folder
-    create_splits_for_folder(base_dir, "dim_reduced_10", train_split, val_split, test_split)
+    create_splits_for_folder(
+        base_dir, "dim_reduced_10", train_split, val_split, test_split
+    )
     # for folder in folders:
     # create_splits_for_folder(folder)
